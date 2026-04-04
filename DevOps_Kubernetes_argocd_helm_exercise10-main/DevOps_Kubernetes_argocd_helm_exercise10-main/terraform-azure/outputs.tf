53ba2dfce23a6115d0f3ebe22a7c25ce0a67e3fa
Nana Asante <contactnanag@gmail.com>
2026-04-04 19:24:42 +0000

    removed exercise10 nested repo

--- DevOps_Kubernetes_argocd_helm_exercise10-main/DevOps_Kubernetes_argocd_helm_exercise10-main/terraform-azure/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public IP address of the VM"
-  value       = azurerm_public_ip.lab_ip.ip_address
-}
-
-output "public_dns" {
-  description = "Public DNS name of the VM"
-  value       = azurerm_public_ip.lab_ip.fqdn
-}
